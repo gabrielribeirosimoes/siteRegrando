4a1cfca37440dc58b88327fa3c8af3a1de621eb1
gabrielribeirosimoes <gabrielribeirosimoes@hotmail.com>
2024-06-26 14:07:06 -0300

    versao final

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Adiciona um evento de clique para os links do menu
+    document.querySelectorAll('nav a').forEach(anchor => {
+        anchor.addEventListener('click', function(e) {
+            e.preventDefault();
+            const targetId = this.getAttribute('href').substring(1);
+            const targetSection = document.getElementById(targetId);
+            targetSection.scrollIntoView({ behavior: 'smooth' });
+        });
+    });
+});
